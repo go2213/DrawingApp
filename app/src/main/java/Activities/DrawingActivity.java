cee2fcbe696e912836d6839db4c1e34fab6a03c1
go2213 <go2213@wayne.edu>
2021-03-29 12:11:48 -0400

    Revert "added undo and redo functionality"
    This reverts commit 973155e6ed612a8ff7df64bcd93eb83a995dc9a3.

--- app/src/main/java/Activities/DrawingActivity.java
+++ app/src/main/java/Activities/DrawingActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.RelativeLayout;
 import com.example.packag.R;
 import Views.CanvasView;
@@ -13,22 +11,14 @@ import Views.CanvasView;
 
 public class DrawingActivity extends AppCompatActivity {
 
-    Button undoButton, redoButton;
     RelativeLayout canvasContainer;
-    CanvasView canvasView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawing);
 
-        undoButton = findViewById(R.id.undoButton);
-        redoButton = findViewById(R.id.redoButton);
-
-
-
-
-        canvasView = new CanvasView(this);
+        CanvasView canvasView = new CanvasView(this);
         canvasView.setBrushColor(Color.MAGENTA);
         canvasView.setBrushStrokeWidth(20f);
 
@@ -36,11 +26,4 @@ public class DrawingActivity extends AppCompatActivity {
         canvasContainer.addView(canvasView);
     }
 
-    public void undoButtonClicked(View view) {
-         canvasView.undoLastStroke();
-    }
-
-    public void redoButtonClicked(View view) {
-        canvasView.redoLastStroke();
-    }
 }
