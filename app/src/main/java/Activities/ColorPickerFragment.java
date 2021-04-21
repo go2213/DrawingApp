70b6f3ecd33943b5e514fd3e3fe148f2ad946eca
go2213 <go2213@wayne.edu>
2021-04-21 14:27:50 -0400

    changed the EditText fields to TextView and moved the color image view to the top of the ColorPickerFragment

--- app/src/main/java/Activities/ColorPickerFragment.java
+++ app/src/main/java/Activities/ColorPickerFragment.java
@@ -1,22 +1,19 @@
 package Activities;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.packag.R;
 
@@ -24,7 +21,7 @@ import com.example.packag.R;
 public class ColorPickerFragment extends Fragment {
 
     private int currentBrushColor;
-    EditText redEditText, greenEditText, blueEditText, opacityEditText;
+    TextView redTextView, greenTextView, blueTextView, opacityTextView;
     BrushColor brushColor;
     SeekBar redSeekBar, greenSeekBar, blueSeekBar, opacitySeekBar;
     ImageView colorIV;
@@ -54,20 +51,20 @@ public class ColorPickerFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_color_picker, container, false);
 
         backImgBtn = v.findViewById(R.id.backImgBtn);
-        redEditText = v.findViewById(R.id.redEditText);
-        greenEditText = v.findViewById(R.id.greenEditText);
-        blueEditText = v.findViewById(R.id.blueEditText);
-        opacityEditText = v.findViewById(R.id.opacityEditText);
+        redTextView = v.findViewById(R.id.redTextView);
+        greenTextView = v.findViewById(R.id.greenTextView);
+        blueTextView = v.findViewById(R.id.blueTextView);
+        opacityTextView = v.findViewById(R.id.opacityTextView);
 
         brushColor = new BrushColor(this.currentBrushColor);
         colorIV = v.findViewById(R.id.colorIV);
         colorIV.setBackgroundColor(brushColor.getBrushColor());
 
 
-        redEditText.setText(String.valueOf(brushColor.getRed()));
-        greenEditText.setText(String.valueOf(brushColor.getGreen()));
-        blueEditText.setText(String.valueOf(brushColor.getBlue()));
-        opacityEditText.setText(String.valueOf(brushColor.getOpacity()));
+        redTextView.setText(String.valueOf(brushColor.getRed()));
+        greenTextView.setText(String.valueOf(brushColor.getGreen()));
+        blueTextView.setText(String.valueOf(brushColor.getBlue()));
+        opacityTextView.setText(String.valueOf(brushColor.getOpacity()));
 
         redSeekBar = v.findViewById(R.id.redSeekBar);
         greenSeekBar = v.findViewById(R.id.greenSeekBar);
@@ -79,40 +76,14 @@ public class ColorPickerFragment extends Fragment {
         blueSeekBar.setProgress(brushColor.getBlue());
         opacitySeekBar.setProgress(brushColor.getOpacity());
 
-        redEditText.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                String rgbInput = redEditText.getText().toString();
-                if(!rgbInput.isEmpty()) {
-                    brushColor.setRed(rgbInput);
-                    redEditText.setText(String.valueOf(brushColor.getRed()));
-                    redSeekBar.setProgress(brushColor.getRed());
-                    colorIV.setBackgroundColor(brushColor.getBrushColor());
-                    redEditText.setSelection(redEditText.getText().length());
-                }
-                return false;
-            }
-        });
 
-        redEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    String rgbInput = redEditText.getText().toString();
-                    if(rgbInput.isEmpty()){
-                        brushColor.setRed(brushColor.getRedFirstDigit());
-                        redEditText.setText(String.valueOf(brushColor.getRed()));
-                        colorIV.setBackgroundColor(brushColor.getBrushColor());
-                    }
-                }
-            }
-        });
+
 
         redSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 brushColor.setRed(progress);
-                redEditText.setText(String.valueOf(brushColor.getRed()));
+                redTextView.setText(String.valueOf(brushColor.getRed()));
                 colorIV.setBackgroundColor(brushColor.getBrushColor());
             }
 
@@ -127,41 +98,14 @@ public class ColorPickerFragment extends Fragment {
             }
         });
 
-        greenEditText.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                String rgbInput = greenEditText.getText().toString();
-                if(!rgbInput.isEmpty()){
-                    brushColor.setGreen(rgbInput);
-                    greenEditText.setText(String.valueOf(brushColor.getGreen()));
-                    greenSeekBar.setProgress(brushColor.getGreen());
-                    colorIV.setBackgroundColor(brushColor.getBrushColor());
-                    greenEditText.setSelection(greenEditText.getText().length());
-                }
 
-                return false;
-            }
-        });
 
-        greenEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    String rgbInput = greenEditText.getText().toString();
-                    if(rgbInput.isEmpty()){
-                        brushColor.setGreen(brushColor.getGreenFirstDigit());
-                        greenEditText.setText(String.valueOf(brushColor.getGreen()));
-                        colorIV.setBackgroundColor(brushColor.getBrushColor());
-                    }
-                }
-            }
-        });
 
         greenSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 brushColor.setGreen(progress);
-                greenEditText.setText(String.valueOf(brushColor.getGreen()));
+                greenTextView.setText(String.valueOf(brushColor.getGreen()));
                 colorIV.setBackgroundColor(brushColor.getBrushColor());
 
             }
@@ -177,40 +121,13 @@ public class ColorPickerFragment extends Fragment {
             }
         });
 
-        blueEditText.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                String rgbInput = blueEditText.getText().toString();
-                if(! rgbInput.isEmpty()) {
-                    brushColor.setBlue(rgbInput);
-                    blueEditText.setText(String.valueOf(brushColor.getBlue()));
-                    blueSeekBar.setProgress(brushColor.getBlue());
-                    colorIV.setBackgroundColor(brushColor.getBrushColor());
-                    blueEditText.setSelection(blueEditText.getText().length());
-                }
-                return false;
-            }
-        });
 
-        blueEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    String rgbInput = blueEditText.getText().toString();
-                    if(rgbInput.isEmpty()){
-                        brushColor.setBlue(brushColor.getBlueFirstDigit());
-                        blueEditText.setText(String.valueOf(brushColor.getBlue()));
-                        colorIV.setBackgroundColor(brushColor.getBrushColor());
-                    }
-                }
-            }
-        });
 
         blueSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 brushColor.setBlue(progress);
-                blueEditText.setText(String.valueOf(brushColor.getBlue()));
+                blueTextView.setText(String.valueOf(brushColor.getBlue()));
                 colorIV.setBackgroundColor(brushColor.getBrushColor());
             }
 
@@ -225,40 +142,13 @@ public class ColorPickerFragment extends Fragment {
             }
         });
 
-        opacityEditText.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                String rgbInput = opacityEditText.getText().toString();
-                if(!rgbInput.isEmpty()) {
-                    brushColor.setOpacity(rgbInput);
-                    opacityEditText.setText(String.valueOf(brushColor.getOpacity()));
-                    opacitySeekBar.setProgress(brushColor.getOpacity());
-                    colorIV.setBackgroundColor(brushColor.getBrushColor());
-                    opacityEditText.setSelection(opacityEditText.getText().length());
-                }
-                return false;
-            }
-        });
 
-        opacityEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus){
-                    String rgbInput = opacityEditText.getText().toString();
-                    if(rgbInput.isEmpty()){
-                        brushColor.setOpacity(brushColor.getOpacityFirstDigit());
-                        opacityEditText.setText(String.valueOf(brushColor.getOpacity()));
-                        colorIV.setBackgroundColor(brushColor.getBrushColor());
-                    }
-                }
-            }
-        });
 
         opacitySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 brushColor.setOpacity(progress);
-                opacityEditText.setText(String.valueOf(brushColor.getOpacity()));
+                opacityTextView.setText(String.valueOf(brushColor.getOpacity()));
                 colorIV.setBackgroundColor(brushColor.getBrushColor());
             }
 
